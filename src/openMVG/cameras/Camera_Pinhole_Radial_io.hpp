4ae1b2f9d6df8b5196775ebe5a5c70966ce7c108
Your Name <you@example.com>
2026-03-06 10:54:51 -0600

    Fix missing file.

--- src/openMVG/cameras/Camera_Pinhole_Radial_io.hpp
+++ src/openMVG/cameras/Camera_Pinhole_Radial_io.hpp
@@ -31,18 +31,14 @@ template <class Archive>
 inline void openMVG::cameras::Pinhole_Intrinsic_Radial_K3::save( Archive & ar ) const
 {
     Pinhole_Intrinsic::save(ar);
-    ar( cereal::make_nvp( "disto_k3", K1_ ) );
-    ar( cereal::make_nvp( "disto_k3", K2_ ) );
-    ar( cereal::make_nvp( "disto_k3", K3_ ) );
+    ar( cereal::make_nvp( "disto_k3", params_ ) );
 }
 
 template <class Archive>
 inline void openMVG::cameras::Pinhole_Intrinsic_Radial_K3::load( Archive & ar )
 {
     Pinhole_Intrinsic::load(ar);
-    ar( cereal::make_nvp( "disto_k3", K1_  ) );
-    ar( cereal::make_nvp( "disto_k3", K2_  ) );
-    ar( cereal::make_nvp( "disto_k3", K3_  ) );
+    ar( cereal::make_nvp( "disto_k3", params_ ) );
 }
 
 CEREAL_REGISTER_TYPE_WITH_NAME(openMVG::cameras::Pinhole_Intrinsic_Radial_K1, "pinhole_radial_k1");
